8512f721dc69c440a71ef0d02b71a833fedde515
dilli <uyra908@gmail.com>
2021-06-16 17:48:57 +0300

    Main is made

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
 $('#parallax-window').parallax({imageSrc: 'img/about_us_first.png'});
 $('#parallax-window-2').parallax({imageSrc: 'img/banner.png'});
+$('#parallax-window-3').parallax({imageSrc: 'img/yeray-sanchez-ZKEjw7oLmQQ-unsplash.jpg'});
+$('#parallax-window-4').parallax({imageSrc: 'img/bannerweston-mackinnon-oiBOjhQb2Ao-unsplash.png'});
+
+let hour = document.querySelector(".banner__hour");
+let date = new Date();
+hour.innerHTML = date.getHours() + ":" + date.getMinutes();
